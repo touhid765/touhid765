6a6a9b49d11f0e716816f3b6ec22a3f9fc10bbc1
touhid765 <160261067+touhid765@users.noreply.github.com>
2024-10-13 18:30:52 +0530

    Delete pages directory

--- pages/ClinicAdmin/AdminDashboard.js
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import React from 'react';
-import Dashboard from '../../components/Dashboard';
-
-const AdminDashboard = () => {
-  return <Dashboard title="Admin Dashboard" />;
-};
-
-export default AdminDashboard;
